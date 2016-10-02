f19c29e7ddf0be7507ad7cabf13a29112ce111f6
raihan-mpower <raihan@mpower-social.com>
2016-10-02 15:56:01 +0600

    first commit

---
+++ collect_app/src/main/java/org/odk/collect/android/activities/webView.java
@@ -0,0 +1,23 @@
+package org.odk.collect.android.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.android.mpower.kobo.R;
+
+/**
+ * Created by raihan on 9/29/16.
+ */
+
+public class webView extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+        String urladdress = getIntent().getStringExtra("urladdress");
+        WebView wv;
+        wv = (WebView) findViewById(R.id.webView1);
+        wv.loadUrl("file:///android_asset/"+urladdress);   // now it will not fail here
+    }
+}
